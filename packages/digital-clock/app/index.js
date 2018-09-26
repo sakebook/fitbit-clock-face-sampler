71f4f18437824e947de31af03269213da9b17999
Shinya Sakemoto <sakebook@gmail.com>
2018-09-26 13:47:37 +0900

    Apply eslint

--- packages/digital-clock/app/index.js
+++ packages/digital-clock/app/index.js
@@ -1,15 +1,14 @@
 import clock from 'clock';
 import document from 'document';
-import * as util from "../common/utils";
+import * as util from '../common/utils';
 
 const clockText = document.getElementById('clock');
 
 function onTick(evt) {
   const hours = evt.date.getHours();
   const minutes = evt.date.getMinutes();
-  clockText.text = util.zeroPad(hours) + ":" + util.zeroPad(minutes);
+  clockText.text = `${util.zeroPad(hours)}:${util.zeroPad(minutes)}`;
 }
 
 clock.granularity = 'minutes';
-clock.ontick = (evt) => onTick(evt);
-
+clock.ontick = evt => onTick(evt);
